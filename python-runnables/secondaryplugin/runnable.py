a89616334da4aba153554df92f89110ce70c7003
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-22 19:24:29 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -1,5 +1,13 @@
 # This file is the actual code for the Python runnable secondaryplugin
 from dataiku.runnables import Runnable
+import os
+import yaml
+from dku_aws.aws_command import AwsCommand
+from dku_utils.cluster import get_cluster_from_dss_cluster, get_cluster_generic_property, set_cluster_generic_property
+from dku_utils.access import _has_not_blank_property
+from dku_kube.kubectl_command import run_with_timeout, KubeCommandException
+from dku_utils.access import _has_not_blank_property, _is_none_or_blank
+
 
 class MyRunnable(Runnable):
     """The base interface for a Python runnable"""
@@ -13,18 +21,50 @@ class MyRunnable(Runnable):
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
-        
+
     def get_progress_target(self):
-        """
-        If the runnable will return some progress info, have this function return a tuple of 
-        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
-        """
         return None
 
     def run(self, progress_callback):
-        """
-        Do stuff here. Can return a string or raise an exception.
-        The progress_callback is a function expecting 1 value: current progress
-        """
-        raise Exception("unimplemented")
-        
+        cluster_data, dss_cluster_settings, dss_cluster_config = get_cluster_from_dss_cluster(self.config['clusterId'])
+
+        if cluster_data is None:
+            raise Exception("No cluster data (not started?)")
+        cluster_def = cluster_data.get("cluster", None)
+        if cluster_def is None:
+            raise Exception("No cluster definition (starting failed?)")
+
+        cluster_id = cluster_def["Name"]
+        kube_config_path = dss_cluster_settings.get_raw()['containerSettings']['executionConfigsGenericOverrides'][
+            'kubeConfigPath']
+        connection_info = dss_cluster_config.get('config', {}).get('connectionInfo', {})
+
+        # needs to set kubectl context
+
+        # needs to set env variable
+
+        # needs AWS commands to query and get subnets
+
+        # this is creating yaml file for each AZ
+        # needs modification so that it can append subnets that are in same AZ
+        # create 1 file per AZ
+        def create_eniconfig(Eniconfig):
+
+            with open(Eniconfig.name + ".yaml", "w") as f:
+                f.write("""apiVersion: crd.k8s.amazonaws.com/v1alpha1
+            kind: ENIConfig
+            metadata:
+              name: """ + Eniconfig.name + """
+                spec:
+              subnet: """ + Eniconfig.subnetId + """    #add multiple subnets 
+              securityGroups:
+              - """ + Eniconfig.securityGroupId)
+            f.close()
+
+    # ability to run kubectl command
+    # kubectl describe nodes | grep 'failure-domain.beta.kubernetes.io/zone'
+    # kubectl set env daemonset aws-node -n kube-system ENI_CONFIG_LABEL_DEF=failure-domain.beta.kubernetes.io/zone
+
+    # node restart?
+
+    raise Exception("unimplemented")
