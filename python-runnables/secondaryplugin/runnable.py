5c201f9672b06d4f07f2c996985b716e37e34e62
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-22 22:42:59 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -41,9 +41,8 @@ class MyMacro(Runnable):
             args = args + ['--region', os.environ['AWS_DEFAULT_REGION']]
 
         c = AwsCommand(args, connection_info)
-        command_outputs.append(c.run())
-        if command_outputs[-1][1] != 0:
-            return make_html(command_outputs)
+        command_outputs.append(str(c.run()))
+
 
         
         subnets = self.config.get('privateSubnets')
