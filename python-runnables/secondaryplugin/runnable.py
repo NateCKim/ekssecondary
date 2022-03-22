84acf4affb3008115283b57dfc883734e7136da5
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-22 19:46:07 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -1,4 +1,11 @@
 from dataiku.runnables import Runnable
+import os
+import yaml
+from dku_aws.aws_command import AwsCommand
+from dku_utils.cluster import get_cluster_from_dss_cluster, get_cluster_generic_property, set_cluster_generic_property
+from dku_utils.access import _has_not_blank_property
+from dku_kube.kubectl_command import run_with_timeout, KubeCommandException
+from dku_utils.access import _has_not_blank_property, _is_none_or_blank
 
 class MyMacro(Runnable):
     def __init__(self, project_key, config, plugin_config):
