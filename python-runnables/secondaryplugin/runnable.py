82f10d0d4499384adf522427693e75fcbf392562
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-22 20:35:49 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -29,5 +29,5 @@ class MyMacro(Runnable):
             'kubeConfigPath']
         connection_info = dss_cluster_config.get('config', {}).get('connectionInfo', {})
 
-        result = cluster_data
+        result = dss_cluster_config
         return result
