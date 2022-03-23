3eac08d38b7230336b6532037d8c2881fda93a23
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-23 19:03:59 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -83,15 +83,14 @@ class MyMacro(Runnable):
         for subnet in subnets:
             t = subdict
             t['enisub'] = subnet
-            for az in subnet:
-                args = ['ec2', 'describe-subnets']
-                args = args + ['--subnet-ids', subnet]
+            args = ['ec2', 'describe-subnets']
+            args = args + ['--subnet-ids', subnet]
             #args = args + ['| jq ".Subnets[].AvailabilityZone"']
-                args = args + ['--query', 'Subnets[0].AvailabilityZone']
-                c = AwsCommand(args, connection_info)
-                command_outputs.append(c.run())
+            args = args + ['--query', 'Subnets[0].AvailabilityZone']
+            c = AwsCommand(args, connection_info)
+            command_outputs.append(c.run())
             t['az'] = command_outputs[1][2].strip().replace('"','')
-            sublist.append(t)
+        sublist.append(t)
         print(t)
             
         #empty list 
