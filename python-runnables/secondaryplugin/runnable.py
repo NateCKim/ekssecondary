505cda977f2d90a0194f665b031c5bd4715aa139
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-23 16:26:49 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -7,6 +7,7 @@ from dku_utils.cluster import get_cluster_from_dss_cluster, get_cluster_generic_
 from dku_utils.access import _has_not_blank_property
 from dku_kube.kubectl_command import run_with_timeout, KubeCommandException
 from dku_utils.access import _has_not_blank_property, _is_none_or_blank
+import boto3
 
 def make_html(command_outputs):
     divs = []
