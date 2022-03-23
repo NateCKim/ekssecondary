b61a47fdcd8557fbf201e4c780cbb1d68c3f5fb8
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-23 14:10:44 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -51,6 +51,8 @@ class MyMacro(Runnable):
 
         c = AwsCommand(args, connection_info)
         command_outputs.append(c.run())
+        if command_outputs[-1][1] != 0:
+            return make_html(command_outputs)
         return command_outputs
 
         
