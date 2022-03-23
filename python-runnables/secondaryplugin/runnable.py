143dce4ab19ab6474d4672225e4dda785abddf29
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-23 19:06:40 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -89,7 +89,7 @@ class MyMacro(Runnable):
             args = args + ['--query', 'Subnets[0].AvailabilityZone']
             c = AwsCommand(args, connection_info)
             command_outputs.append(c.run())
-            print(command_outputs[1][2])
+            print(command_outputs[1])
             t['az'] = command_outputs[1][2].strip().replace('"','')
         sublist.append(t)
         print(t)
