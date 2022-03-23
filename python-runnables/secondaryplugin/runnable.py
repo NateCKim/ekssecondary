ce91f8ff67130874074b46e7d794ad4d528af883
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-23 14:11:02 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -52,8 +52,7 @@ class MyMacro(Runnable):
         c = AwsCommand(args, connection_info)
         command_outputs.append(c.run())
         if command_outputs[-1][1] != 0:
-            return make_html(command_outputs)
-        return command_outputs
+            return command_outputs
 
         
        # subnets = self.config.get('privateSubnets')
