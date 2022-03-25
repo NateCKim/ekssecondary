4e2862638386695fea06c5d24eb22534d9199c39
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-25 04:05:30 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -136,7 +136,7 @@ class MyMacro(Runnable):
         
         args = ['ec2', 'describe-instances']
         args = args + ['--query "Reservations[].Instances[].InstanceId"']
-        args = args + ['--filters "Name=eks:cluster-name,Values="', '--output text']
+        args = args + ['--filters "Name=eks:cluster-name,Values=nate4"', '--output text']
         c = None
         c = AwsCommand(args, connection_info)
         command_outputs = []
