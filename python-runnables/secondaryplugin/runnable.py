83c65f96c977562dd5adb1e0afb8afa2e1de97fa
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-25 18:43:47 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -145,7 +145,7 @@ class MyMacro(Runnable):
         print(command_outputs)
         
         r = {}
-        r['list'] = command_outputs[0][2].strip().replace('"','')
+        r['list'] = command_outputs[0][2].strip().replace('\t','')
         print(r)
         #args = ['ec2', 'terminate-instances', '--instance-ids', '{}'.format(r)]
         #args = ['ec2', 'terminate-instances', '--instance-ids', '"$(aws ec2 describe-instances']
