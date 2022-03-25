9558894284125c0a04f0b15126984b1838f004b8
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-25 19:18:26 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -150,7 +150,7 @@ class MyMacro(Runnable):
 
         for kk in r['list']:
             args = []
-            args = ['ec2', 'terminate-instances', '--instance-ids', kk.split(' ')]
+            args = ['ec2', 'terminate-instances', '--instance-ids', kk]
             c = None
             c = AwsCommand(args, connection_info)
             command_outputs = []
