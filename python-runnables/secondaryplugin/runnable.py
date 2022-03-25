f3404250ac5853a544d7d006900b03999494409f
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-25 04:11:09 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -132,7 +132,7 @@ class MyMacro(Runnable):
         #pass in self.config to get nodegroup. Set the Desire (if desire is > 0 scale to 0 )
         #aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:tagkey,Values=tagvalue" --output text)
         t = self.config['clusterId']
-        remove_space = [x.strip(' ') for x in t]
+        remove_space = [.strip(' ')]
         print(remove_space)
         
         args = ['ec2', 'describe-instances']
