8633a2fee6aaba9c36999f4cd701380b9e77ab88
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-25 02:19:52 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -139,7 +139,7 @@ class MyMacro(Runnable):
         c = AwsCommand(args, connection_info)
         command_outputs = []
         command_outputs.append(c.run())
-        print(command_outputs[0][2])
+        print(command_outputs[0][3])
         
         
         #can get rid of it afterwards. Only used for testing syntax as successful return will not generate the output
