63af75f0c8f1ba6989c20ad00dea841ccdd87908
nate.kim@dataiku.com <nate.kim@dataiku.com>
2022-03-27 09:23:04 +0000

    Edited files

--- python-runnables/secondaryplugin/runnable.py
+++ python-runnables/secondaryplugin/runnable.py
@@ -146,7 +146,8 @@ class MyMacro(Runnable):
    
 
         #rsave = command_outputs[0][2].split('\t')
-        rsave = command_outputs[0][2].strip().replace('"','').replace('\n','').replace(' ','')
+        #rsave = command_outputs[0][2].strip().replace('"','').replace('\n','').replace(' ','')
+        rsave = command_outputs[0][2].strip()
         #rsave = command_outputs[0][2]
         print(rsave)
        
